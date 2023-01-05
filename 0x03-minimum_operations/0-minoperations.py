37081bcc461b8307bbe1e868632cdf79ea0b8165
Achref-haddaji <haddaji.achref19@gmail.com>
2023-01-05 18:18:18 +0100

    task0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -10,11 +10,18 @@ def minOperations(n):
     a method that determines the least amount of
     operation to get the desired outcome.
     """
+   newvalue = n
+    i = 2
+    rest = 0
     if (not isinstance(n, int)):
         return 0
-    count = 0
-    for i in range(2, n):
-        while (n % i == 0):
-            count += i
-            n = n / i
-    return count
+    if n < 2:
+        return 0
+    while newvalue != 1:
+        if newvalue % i == 0:
+            newvalue = newvalue / i
+            rest += i
+        else:
+            i = i + 1
+    return rest
+
