00049920541747a660f289b1dc40c2314a7755e0
Achref-haddaji <haddaji.achref19@gmail.com>
2023-01-05 18:23:13 +0100

    task0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,14 +7,22 @@
 
 def minOperations(n):
     """
-    a method that determines the least amount of
-    operation to get the desired outcome.
+    method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file
     """
+
     if (not isinstance(n, int)):
         return 0
+
+    if (n < 2):
+        return 0
+
     count = 0
-    for i in range(2, n):
-        while (n % i == 0):
+    i = 2
+    while i <= n:
+        if n % i == 0:
             count += i
             n = n / i
-    return count
+            i = i - 1
+        i = i + 1
+    return int(count)
