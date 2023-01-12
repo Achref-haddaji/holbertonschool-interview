22c0f2f744111bf53e991cb426c41425b3f97e99
Achref-haddaji <haddaji.achref19@gmail.com>
2023-01-12 21:03:34 +0100

    header

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid_per(int grid[3][3]);
+void pop(int grid[3][3], int i, int j);
+void fill(int grid1[3][3], int grid2[3][3]);
+
+#endif
