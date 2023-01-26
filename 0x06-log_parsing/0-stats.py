39931dfd0035de26935284c364a7c53ccefffc7b
Achref-haddaji <haddaji.achref19@gmail.com>
2023-01-26 21:11:40 +0100

    task0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,16 +3,16 @@
 import fileinput
 
 
-status_code  = {'200': 0, '301': 0, '400': 0,
+status_code = {'200': 0, '301': 0, '400': 0,
                '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 file_size = 0
 count = 0
 
 
 def print_output():
-    """ Print codes and numbers"""
+    """ print the status acording"""
     print("File size: {}".format(file_size))
-    for a, b in sorted(status_code .items()):
+    for a, b in sorted(status_code.items()):
         if b != 0:
             print("{}: {}".format(a, b))
 
@@ -24,8 +24,8 @@ if __name__ == "__main__":
         for i in fileinput.input():
             try:
                 data = i.split(" ")
-                if data[-2] in status_code .keys():
-                    status_code [data[-2]] += 1
+                if data[-2] in status_code.keys():
+                    status_code[data[-2]] += 1
                 file_size += int(data[-1])
                 count += 1
             except Exception:
