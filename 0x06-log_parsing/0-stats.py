018929046160fe1c51c1bc7cdf9c8c64401665dd
Achref-haddaji <haddaji.achref19@gmail.com>
2023-01-26 21:02:11 +0100

    task0

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" program that read line from stdin"""
+import fileinput
+
+
+errors = {'200': 0, '301': 0, '400': 0,
+               '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+file_size = 0
+count = 0
+
+
+def print_output():
+    """ print the status acording"""
+    print("File size: {}".format(file_size))
+    for a, b in sorted(errors.items()):
+        if b != 0:
+            print("{}: {}".format(a, b))
+
+
+if __name__ == "__main__":
+    """main function"""
+
+    try:
+        for i in fileinput.input():
+            try:
+                data = i.split(" ")
+                if data[-2] in errors.keys():
+                    errors[data[-2]] += 1
+                file_size += int(data[-1])
+                count += 1
+            except Exception:
+                pass
+            if count % 10 == 0:
+                print_output()
+        print_output()
+    except KeyboardInterrupt:
+        print_output()
+        raise
