8c5952313432326573cd05cfae81fe6ef9f10b17
Achref-haddaji <haddaji.achref19@gmail.com>
2023-05-16 14:57:11 +0100

    header

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void the_print(int *array, int l, int r);
+int advanced_binary_counter(int *array, int value, int l, int r);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
