95e83fc297cad9abfcdf7dbbff009aeb525337a0
Achref-haddaji <haddaji.achref19@gmail.com>
2023-05-25 15:22:51 +0100

    header

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+int is_digit(char c);
+int is_positive_number(char *str);
+int string_length(char *str);
+void print_error(void);
+void print_number(char *number);
+char *multiply(char *num1, char *num2);
+
+#endif /* HOLBERTON_H */
