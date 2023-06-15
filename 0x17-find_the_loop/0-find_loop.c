5ef14147b39e6d82978215b2b8deaed83c0c2d4b
Achref-haddaji <haddaji.achref19@gmail.com>
2023-06-15 16:37:29 +0100

    task0

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -4,34 +4,34 @@
  * find_listint_loop - Finds the loop in a linked list.
  * @head: Pointer to the head of the linked list.
  *
- * Return: The address of the node where the loop starts, or NULL if there is no loop.
+ * Return: The address of the node where the loop starts.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *tortoise, *hare;
+	listint_t *tortoise, *hare;
 
-    if (head == NULL || head->next == NULL)
-        return NULL;
+	if (head == NULL || head->next == NULL)
+		return (NULL);
 
-    tortoise = head->next;
-    hare = head->next->next;
+	tortoise = head->next;
+	hare = head->next->next;
 
-    while (hare && hare->next)
-    {
-        if (tortoise == hare)
-        {
-            tortoise = head;
-            while (tortoise != hare)
-            {
-                tortoise = tortoise->next;
-                hare = hare->next;
-            }
-            return tortoise;
-        }
+	while (hare && hare->next)
+	{
+	if (tortoise == hare)
+	{
+		tortoise = head;
+			while (tortoise != hare)
+		{
+			tortoise = tortoise->next;
+			hare = hare->next;
+			}
+			return (tortoise);
+		}
 
-        tortoise = tortoise->next;
-        hare = hare->next->next;
-    }
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
 
-    return NULL;
+	return (NULL);
 }
