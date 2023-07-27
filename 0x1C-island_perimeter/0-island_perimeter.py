00c97f34aefe83694a8ecc3e84b5b50b6f0d6213
Achref-haddaji <haddaji.achref19@gmail.com>
2023-07-27 14:58:40 +0100

    task0

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,30 +1,20 @@
-def island_perimeter(grid):
-    """
-    Calculate the perimeter of the island described in the grid.
+#!/usr/bin/python3
 
-    Returns:
-        int: The perimeter of the island.
+""" Function to find the perimeter of an island """
 
-    Constraints:
-        - Cells are connected horizontally/vertically (not diagonally).
-        - The grid is completely surrounded by water.
-        - There is only one island (or nothing).
-        - The island doesn’t have “lakes”
-    """
-    if not grid or not grid[0]:
-        return 0
 
+def find_island_perimeter(grid):
+    """ Function to find the perimeter of an island """
     perimeter = 0
-    rows, cols = len(grid), len(grid[0])
-
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 1:
-                perimeter += 4
-
-                if r > 0 and grid[r - 1][c] == 1:
-                    perimeter -= 2
-                if c > 0 and grid[r][c - 1] == 1:
-                    perimeter -= 2
-
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                if x - 1 < 0 or grid[x - 1][y] == 0:
+                    perimeter += 1
+                if x + 1 >= len(grid) or grid[x + 1][y] == 0:
+                    perimeter += 1
+                if y - 1 < 0 or grid[x][y - 1] == 0:
+                    perimeter += 1
+                if y + 1 >= len(grid[x]) or grid[x][y + 1] == 0:
+                    perimeter += 1
     return perimeter
