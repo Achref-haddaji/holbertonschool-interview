f1e95afbe8d651f781caa300f9314bdcbfa6f1fb
Achref-haddaji <haddaji.achref19@gmail.com>
2023-07-27 14:39:38 +0100

    task0

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,19 +1,9 @@
+#!/usr/bin/python3
+
+""" Function  that returns the perimeter of the island described in grid """
+
+
 def island_perimeter(grid):
-    """
-    Calculate the perimeter of the island described in the grid.
-
-    Args:
-        grid (list of list of int): A rectangular grid where 0 represents water and 1 represents land.
-
-    Returns:
-        int: The perimeter of the island.
-
-    Constraints:
-        - Cells are connected horizontally/vertically (not diagonally).
-        - The grid is completely surrounded by water.
-        - There is only one island (or nothing).
-        - The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
-    """
     if not grid or not grid[0]:
         return 0
 
@@ -23,9 +13,8 @@ def island_perimeter(grid):
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-                perimeter += 4  # Start with the maximum perimeter for each land cell.
+                perimeter += 4
 
-                # Check adjacent cells and reduce the perimeter for each connected side.
                 if r > 0 and grid[r - 1][c] == 1:
                     perimeter -= 2
                 if c > 0 and grid[r][c - 1] == 1:
