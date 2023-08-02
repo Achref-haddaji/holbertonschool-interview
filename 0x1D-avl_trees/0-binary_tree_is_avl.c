0f266595fdb38bff57c4a6a844d179a4a55b0b99
Achref-haddaji <haddaji.achref19@gmail.com>
2023-08-02 17:47:31 +0100

    task0

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -1,51 +1,39 @@
 #include "binary_trees.h"
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define ABS(x) ((x) > 0 ? (x) : (-(x)))
 
 /**
- * max - Returns the maximum of two integers
- * @a: First integer
- * @b: Second integer
- * Return: The maximum of a and b
+ * is_avl - recursively checks if tree is AVL
+ * @tree: pointer to current root
+ * @min: current min val
+ * @max: current max val
+ * @height: height of this tree by address
+ * Return: true if is AVL else 0
  */
-int max(int a, int b)
+int is_avl(const binary_tree_t *tree, int min, int max, int *height)
 {
-	return (a > b ? a : b);
+	int h1 = 0, h2 = 0;
+
+	if (!tree)
+		return (1);
+	if (tree->n <= min || tree->n >= max)
+		return (0);
+	if (!is_avl(tree->left, min, tree->n, &h1) ||
+			!is_avl(tree->right, tree->n, max, &h2))
+		return (0);
+	*height = MAX(h1, h2) + 1;
+	return (ABS(h1 - h2) <= 1);
 }
-
 /**
- * binary_tree_height - Calculates the height of a binary tree
- * @tree: Pointer to the root node of the tree
- * Return: The height of the tree, or 0 if tree is NULL
- */
-int binary_tree_height(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (-1);
-
-	return (1 + max(binary_tree_height(tree->left),
-			binary_tree_height(tree->right)));
-}
-
-/**
- * binary_tree_is_avl - Checks if a binary tree is a valid AVL Tree
- * @tree: Pointer to the root node of the tree to check
- * Return: 1 if tree is a valid AVL Tree, 0 otherwise
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if AVL else 0
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	int left_height, right_height, balance_factor;
+	int height;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	balance_factor = abs(left_height - right_height);
-
-	if (balance_factor <= 1 &&
-	    binary_tree_is_avl(tree->left) &&
-	    binary_tree_is_avl(tree->right))
-		return (1);
-
-	return (0);
+	return (is_avl(tree, INT_MIN, INT_MAX, &height));
 }
