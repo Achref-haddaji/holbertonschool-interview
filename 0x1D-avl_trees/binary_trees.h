caae6ca65e9ab6861c2e1b47af04ae10d3cc8b3a
Achref-haddaji <haddaji.achref19@gmail.com>
2023-08-03 17:52:36 +0100

    header

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -1,8 +1,6 @@
-#ifndef __BINARY_TREES_H_
-#define __BINARY_TREES_H_
-
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 #include <stdio.h>
-#include <stdlib.h>
 #include <limits.h>
 /**
  * struct binary_tree_s - Binary tree node
@@ -21,11 +19,7 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
 typedef struct binary_tree_s avl_t;
-
 int binary_tree_is_avl(const binary_tree_t *tree);
-binary_tree_t *binary_tree_node(binary_tree_t *, int);
-void binary_tree_print(const binary_tree_t *);
 
-#endif  /* __BINARY_TREES_H_ */
+#endif
