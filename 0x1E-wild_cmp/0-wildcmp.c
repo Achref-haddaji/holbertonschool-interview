99223cff93bc507ad795549f0ecf391a2f28142c
Achref-haddaji <haddaji.achref19@gmail.com>
2023-08-14 15:33:09 +0100

    task0

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings the second string may contain wildcard "*"
+ * @s1: first string
+ * @s2: second string
+ * Return: 1  or 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	if (!*s1)
+	{
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (!*s2);
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	if (*s1 != *s2)
+		return (0);
+	return (wildcmp(s1 + 1, s2 + 1));
+}
